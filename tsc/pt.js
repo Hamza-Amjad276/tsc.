13e89736f855107a16981ca47baa5cafbddba746
hamzaharis 37 <hamzaharis298@gmail.com>
2024-06-26 10:41:18 +0500

    Add files via upload

---
+++ tsc/pt.js
@@ -0,0 +1,156 @@
+"use strict";
+//console.log("hello world");
+//var myage = 14;
+//console.log(myage);
+//var harisage = 12;
+//console.log(harisage);
+//var myname = 'hamza';
+//console.log(myname);
+// DATA TYPES premative datatypes
+// string 
+// numbers
+//boolean
+//undefined
+//null
+//bigint
+// non premative data types 
+// funtions
+//array
+// objects
+//variable
+//var,let,const
+//concatenation  
+//   + "  ,  "  +
+// exponentation
+//let p = 2 ** 4//16
+//console.log(p)
+// MODULES
+//let m = 6 % 2 // 0 reminder return krta hai
+//console.log(m);
+// unary operators  ++,--
+// increament or decreament
+//let z =  7;// 8//9
+//let v = 5; //4 //3 //4
+//let h = v++ + z-- + ++z + --v + --z + ++z //
+//console.log(7+1 , 5-1 , 8+1 , 9-1 , 4-1 , 3+1)
+//console.log(8+4+9+8+3+4);// 36
+//increament
+//let g = 7
+//g++ // +1
+//++g // +1
+//console.log(++v)
+//console.log(v)
+//dereament
+//let f = 5
+//f-- // -1
+//--f // -1
+//console.log(f)
+// combining operators  // BODMAS brackets division multiplication addition subtraction
+//let i = 2*5-4+3/3+(2+2)
+//console.log(2*5-4+3/3-1)
+//console.log(2*5-4+1+4)
+//console.log(10-4+1+4)
+//console.log(10-9); //1
+// bmi calculator
+// weight
+// height
+// bmi formila
+//let weight = 50;
+//let height  =  5.3
+//let square = height ** 2
+//let bmi = weight / square;
+//console.log(bmi. toFixed (2));
+// comparison operators
+// == , != , <  ,  > , >= , <= ,===
+//let friend = "haris"
+//let newfriend = "aqib"
+//let num1 = 10
+//let num2 = 21
+//let result = typeof friend === typeof num1
+//let num3 = num1 >= num2 // true //false // true //false
+//console.log(friend  !=  newfriend); //true
+// Eid wishing project 
+//for(let count = 0; count <=1000; count++){
+// count % 2 === 0? console.log(`${count} Eid mubarak `) : console.log(`${count} khair mubarak`)   
+//}
+// data types
+//let myage3: string = `7777`
+//myage3 = '68'
+//console.log(myage3);
+//let num = 77 > 65
+//console.log(num)
+//var myage4 = "44"
+//var myage8 = "35"
+//var myage2 = "21"
+//var aliage = "12"
+//console.log(myage4);
+//let number : number = 2367
+//number = 67
+//console.log(number);
+// OPERATORS IN TYPESCRIPT
+// ARITHMETIC OPERATORS
+// ADDITON, SUBTRACTION ,MULTI, DIVISION , modulus
+//let a = 4
+//let b = 3
+//console.log(a+b);
+//let a = 8
+//let b = 3
+//let c = a+b
+//console.log(c);
+//let a = 5
+//a += 4
+//console.log(a);
+// subtraction 
+//let a = 8
+//let b = 6
+//console.log(a-b);
+//let a = 66
+//let b = 3
+//let c = a-b
+//console.log(c);
+//let a = 9
+//a -= 4
+//console.log(a);
+//multiplication
+//let a = 4
+//let b = 3
+//console.log(a*b);
+//let a = 8
+//let b = 2
+//let c = a*b
+//console.log(c);
+// division
+//let a = 8
+//let b = 2
+//let c = a/b
+//console.log(c);
+// modulus operator  // reminder deta hai
+//let a = 7%3
+//console.log(a);
+// exponentation
+//let p = 2 ** 4//16
+//console.log(p)
+// unary operators  ++,--
+// increament or decreament
+//let z =  7;// 8//9
+//let v = 5; //4 //3 //4
+//let h = v++ + z-- + ++z + --v + --z + ++z //
+//console.log(7+1 , 5-1 , 8+1 , 9-1 , 4-1 , 3+1)
+//console.log(8+4+9+8+3+4);// 36
+//comparison operator
+// == , != , <  ,  > , >= , <= ,===
+//let friend = "haris"
+//let newfriend = "aqib"
+//let num1 = 10
+//let num2 = 21
+//let result = typeof friend === typeof num1
+//let num3 = num1 >= num2 // true //false // true //false
+//console.log(friend  !=  newfriend); //true
+// combining operators  // BODMAS brackets division multiplication addition subtraction
+//let i = 2*5-4+3/3+(2+2)
+//console.log(2*5-4+3/3-1)
+//console.log(2*5-4+1+4)
+//console.log(10-4+1+4)
+//console.log(10-9); //1
+//represent assignment operator
+//+= , -+ , *= /+ , ** , %=
